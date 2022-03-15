9e108d61cd3eb7345690921662b28ce50bf10fb0
Hollin <hollinisme@gmail.com>
2022-03-16 00:16:47 +0800

    Test input_parser

--- SDK/SDK_C++/CodeCraft-2022/src/CodeCraft-2022.cpp
+++ SDK/SDK_C++/CodeCraft-2022/src/CodeCraft-2022.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 #include "input_parser.h"
 
+static bool local = true;
+
 int main() {
-  std::cout << "Hello world!" << std::endl;
-  InputParser parser;
-  parser.Parse();
+  InputParser *parser;
+  if(local){
+    parser = new InputParser("../data/config.ini", "../data/demand.csv", "../data/qos.csv", "../data/site_bandwidth.csv");
+  }else{
+    parser = new InputParser();
+  }
+  parser->Parse();
+  auto cnm = parser->GetClientNodeMap();
+  auto enm = parser->GetEdgeNodeMap();
+  for(auto &[name, node] : cnm){
+    std::cout << node->ToString() << std::endl;
+  }
+
+  for(auto &[name, node] : enm){
+    std::cout << node->ToString() << std::endl;
+  }
   return 0;
 }
