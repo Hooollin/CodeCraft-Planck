d82d30caab8d9ede0033cca84f32960147087eea
linmiaomiao <464453990@qq.com>
2022-03-17 18:08:21 +0800

    有错策略all

---
+++ SDK/SDK_C++/CodeCraft-2022/src/implement/output_parser.cpp
@@ -0,0 +1,58 @@
+#include "output_parser.h"
+
+OutputParser::OutputParser(int model) {
+  assert(model >= 1 && model <= 3);
+  if (model == 1) {
+    solution_ = online_pre_ + solution_suf_;
+  } else if (model == 2) {
+    solution_ = linux_pre_ + solution_suf_;
+  } else {
+    solution_ = windows_pre_ + solution_suf_;
+  }
+  client_list_.clear();
+}
+
+void OutputParser::SetAllDay(int day) {
+  day_ = day;
+  output_data_.clear();
+  output_data_.resize(day);
+}
+
+void OutputParser::SetOutput(
+    int day,
+    std::unordered_map<std::string, std::unordered_map<std::string, int> >
+        day_distribution) {
+  output_data_[day] = day_distribution;
+}
+
+void OutputParser::SetOutput(
+    int day, std::string client,
+    std::unordered_map<std::string, int> day_client_distribution) {
+  output_data_[day][client] = day_client_distribution;
+}
+
+void OutputParser::SetOutput(int day, std::string client, std::string edge,
+                             int distribution) {
+  output_data_[day][client][edge] = distribution;
+}
+
+void OutputParser::StandradOutput() {
+  ofs_.clear();
+  ofs_.open(solution_, std::ios::out);
+  assert(ofs_.is_open());
+  for(auto &day_distribution : output_data_){
+    for(auto &client : client_list_){
+      ofs_<<client<<":";
+      bool flag = false;
+      for(auto &distribution : day_distribution[client]){
+        std::string edge = distribution.first;
+        int distri = distribution.second;
+        if(distri == 0) continue;
+        if(flag) ofs_<<",";
+        ofs_<<"<"<<edge<<","<<distri<<">";
+        flag = true;
+      }
+      ofs_<<std::endl;
+    }
+  }
+}
---
+++ SDK/SDK_C++/CodeCraft-2022/src/include/output_parser.h
@@ -0,0 +1,51 @@
+#pragma once
+#include <cassert>
+#include <fstream>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <iostream>
+
+class OutputParser {
+ public:
+  OutputParser(int model);
+
+  void SetClientList(std::unordered_set<std::string> client_list) {
+    client_list_ = client_list;
+  }
+
+  void AddClient(std::string client) { client_list_.insert(client); }
+
+  void SetAllDay(int day);
+
+  void SetOutput(
+      int day,
+      std::unordered_map<std::string, std::unordered_map<std::string, int> >
+          day_distribution);
+
+  void SetOutput(int day, std::string client,
+                 std::unordered_map<std::string, int> day_client_distribution);
+
+  void SetOutput(int day, std::string client, std::string edge,
+                 int distribution);
+
+  void StandradOutput();
+
+ private:
+  std::string linux_pre_ = "..";
+  std::string windows_pre_ = "../../..";
+  std::string online_pre_ = "";
+  std::string solution_suf_ = "/output/solution.txt";
+  std::string solution_;
+
+  int day_;
+
+  std::unordered_set<std::string> client_list_;
+
+  std::ofstream ofs_;
+
+  std::vector<
+      std::unordered_map<std::string, std::unordered_map<std::string, int> > >
+      output_data_;
+};
